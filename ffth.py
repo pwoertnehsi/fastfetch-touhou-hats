81ae1285c3b6d135a0359f67ea2acd3cfdde2aea
pwoertnehsi <sewertenshi@yandex.ru>
2026-01-23 10:44:48 +0400

    Script robustness

--- ffth.py
+++ ffth.py
@@ -5,9 +5,16 @@ import subprocess
 import shutil
 from pathlib import Path
 
+script_root = os.path.abspath(os.path.dirname(__file__))
+if script_root.endswith("/ffth/_internal"):
+    script_root = script_root.rstrip("/ffth/_internal")
+
+if Path((script_root + "/screenshots")).exists():
+    shutil.rmtree(script_root + "/screenshots")
+
 ffpath = Path.home() / ".config/fastfetch/"
 
-if (ffpath / "config.jsonc").exists():
+if ((ffpath / "config.jsonc").exists() and not((ffpath / "config.jsonc").exists())):
     while True:
         backup_prompt = input("make a backup of current config.jsonc? [y/n] ")
         if ((backup_prompt == "y") or (backup_prompt == "Y")): 
@@ -20,7 +27,8 @@ if (ffpath / "config.jsonc").exists():
             
         if (((backup_prompt == "y") or (backup_prompt == "Y")) or ((backup_prompt == "y") or (backup_prompt == "Y"))):
             break
-            
+elif ((ffpath / "config.jsonc").exists() and ((ffpath / "config.jsonc").exists())):
+    pass
 else:
     gen_config = subprocess.run(["fastfetch", "--gen-config"], capture_output=True, text=True)
     print(f"generating config file {str(ffpath)}.jsonc\n" + gen_config.stdout)
@@ -29,21 +37,22 @@ ffconfigpath = ffpath / "config.jsonc"
 
 hats = {}
 
-print("available hats:")
+print("available hats:\n")
 
-for path, dirname, filename in os.walk(ffpath / "fastfetch-touhou-hats/hats"):
-    for item in filename:
-        if item.endswith(".jsonc"):
-            #checking if an ascii art is present
-            if item.replace(".jsonc", ".ascii") in filename:
-                hatname = item.split(".")[0]
-                hats[hatname] = ffpath / "fastfetch-touhou-hats/hats" / hatname
-                print(hatname)
-            else:
-                hatname = item.split(".")[0]
-                print(f"\x1b[33m{hatname} (ascii not found)\x1b[0m")
+for path, dirname, filename in os.walk(Path(script_root + "/hats")):
+        for item in filename:
+            if item.endswith(".jsonc"):
+                #checking if an ascii art is present
+                if item.replace(".jsonc", ".ascii") in filename:
+                    hatname = item.split(".")[0]
+                    hats[hatname] = path
+                    print(hatname)
+                else:
+                    hatname = item.split(".")[0]
+                    hats[hatname] = path
+                    print(f"\x1b[33m{hatname} (ascii not found)\x1b[0m")
 
-chosen_hat = input("choose the desired hat: ")
+chosen_hat = input("\nchoose the desired hat: ")
 
 if chosen_hat not in hats:
     #errors with code 1 if user input is invalid
@@ -56,7 +65,7 @@ hatconfig = None
 with open(ffconfigpath) as f:
     ffconfig = json5.load(f)
 
-with open(hats[chosen_hat] / (chosen_hat + ".jsonc")) as f:
+with open(f"{hats[chosen_hat]}/{chosen_hat}.jsonc") as f:
     hatconfig = json5.load(f)
 
 def deep_merge(base: dict, override: dict) -> dict:
@@ -74,7 +83,7 @@ def deep_merge(base: dict, override: dict) -> dict:
 
 ffconfignew = deep_merge(ffconfig, hatconfig)
 
-ffconfignew["logo"]["source"] = str(hats[chosen_hat] / (chosen_hat + ".ascii"))
+ffconfignew["logo"]["source"] = f"{hats[chosen_hat]}/{chosen_hat}.ascii"
 ffconfignew["display"]["brightColor"] = "false"
 
 with open(ffconfigpath, "w") as f:
